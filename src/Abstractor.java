2ee6c8b98577e573ec4fcadab9a73db88ef1be02
Brendan O'Connor <brendan.oconnor.913@gmail.com>
2017-01-16 14:11:43 -0600

    Add metadata functionality for one instance, need to expand to any number of metadata columns

--- src/Abstractor.java
+++ src/Abstractor.java
@@ -8,6 +8,7 @@ import java.util.Vector;
  * Created by brendan on 10/9/16.
  */
 public class Abstractor {
+    // function to return vector containing header for each column
     private static Vector<String> getHeader(String fname) {
         Vector<String> header = new Vector<String>();
         try {
@@ -27,15 +28,16 @@ public class Abstractor {
 
     public static void main(String args[]) {
         // TODO: after flow is working go back through and work on quality of triples created
-        // For some reason template handles empty strings by inputting a random seq of alphanum chars
         String filename = args[0];
         String graphname = args[1];
         String context = args[2]; // for quad creation
         try {
+            // process column headers (map to resources and gather metadata)
             Vector<String> tmpheader = getHeader(filename);
             AttributeHandler ah = new AttributeHandler(graphname);
             Vector<Attribute> aMeta = ah.getUnits(tmpheader, ah.findAttributes(tmpheader));
 
+            // get header row
             Reader in = new FileReader(filename);
             Iterable<CSVRecord> recordForHeader = CSVFormat.EXCEL.parse(in);
             CSVRecord headerRecord = recordForHeader.iterator().next();
@@ -73,17 +75,30 @@ public class Abstractor {
                     }
                     subLit = sb.toString();
                 }
+                // Create subject and first triple
                 subj = "<http://umkc.edu/subject#${" + subLit + "}>";
                 subject.append(subj + " <http://rdf/label> <" + aMeta.get(num).resource + "> .\n");
             }
-            for(int i = 0; i < aMeta.size(); i++) {
-                if (i == num) { // modify if mult subj cols
+
+            for (int i = 0; i < aMeta.size(); i++) {
+                Attribute a = aMeta.get(i);
+                if (i == num || (a.unitcol.equals(a.ISUNIT))) { // modify if mult subj cols
                     continue;
                 }
-                subject.append(subj + " <" + aMeta.get(i).resource + "> \"${" + headerRecord.get(i).trim() + "}\" .\n");
+                else if (!a.unitcol.equals(a.NOUNIT)) {
+                    subject.append(subj + " <" + aMeta.get(i).resource + "> _:metadata .\n");
+                    subject.append("_:metadata" + " <" + aMeta.get(i).resource +
+                            "> \"${" + headerRecord.get(i).trim() + "}\" .\n");
+                    subject.append("_:metadata" + " <" + aMeta.get(a.unitcol).resource +
+                            "> \"${" + headerRecord.get(a.unitcol).trim() + "}\" .\n");
+                }
+                else {
+                    subject.append(subj + " <" + aMeta.get(i).resource + "> \"${" +
+                            headerRecord.get(i).trim() + "}\" .\n");
+                }
             }
 
-            // TODO: after string construction working properly output to file
+            // Outputs created template to file
             PrintStream out = new PrintStream(new FileOutputStream("triptemp.nt"));
             System.setOut(out);
             System.out.print(subject.toString());
